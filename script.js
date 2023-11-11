faa1027e40f087da06e221d6cfc2db6625b3a18a
Jose Junior Navarro Meneses <68346160+josejrnm@users.noreply.github.com>
2023-11-10 23:32:25 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
-const height = document.querySelector('#height');
-const consult = document.querySelector('#consult')
+const selectImage = document.querySelector('.select-image');
 const inputFile = document.querySelector('#file');
 const imgArea = document.querySelector('.img-area');
 
-imgArea.addEventListener('click', function () {
+selectImage.addEventListener('click', function () {
 	inputFile.click();
 })
 
 inputFile.addEventListener('change', function () {
 	const image = this.files[0]
-	if(image.size < 4000000) {
+	if(image.size < 2000000) {
 		const reader = new FileReader();
 		reader.onload = ()=> {
 			const allImg = imgArea.querySelectorAll('img');
@@ -20,24 +19,9 @@ inputFile.addEventListener('change', function () {
 			imgArea.appendChild(img);
 			imgArea.classList.add('active');
 			imgArea.dataset.img = image.name;
-            console.log(image);
 		}
 		reader.readAsDataURL(image);
 	} else {
-		alert("Image size more than 5MB");
+		alert("Image size more than 2MB");
 	}
-})
-
-consult.addEventListener('click',function (params) {
-    fetch('http://127.0.0.1:8000/img/photos', {
-            method: 'POST',
-            headers: {
-                'Content-type': 'application/json'
-            },
-            body: JSON.stringify({
-            })
-        }).then(() => {
-            setTextName('')
-            setTextPassword('')
-        })    
 })
